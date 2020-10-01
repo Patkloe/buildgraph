3b18a2b5e0225843dd4cc263def4ccd6625371cc
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-10-01 07:42:57 -0500

    Update buildgraph.js

--- buildgraph.js
+++ buildgraph.js
@@ -11,29 +11,43 @@ class Graph{
   this.listadj.get(w).push(v);
  }
  printgraph(){
-  var getkeys = this.listadj.keys(); // recupere keys as : exemple [A[B,C,D,E],B[....], .....]
+  var getkeys = this.listadj.keys();
   for(var i of getkeys){
-    var conc = " ";
-    var getsommets = this.listadj.get(i);  // pour chaque sommet
-    for(var j of getsommets)
-     conc = conc + j;
-  console.log(i + " " + "->" + " "+ conc);
+      var conc = " ";
+      var listsommet = this.listadj.get(i);
+      for(var j of listsommet)
+        conc = conc + j;
+   console.log(i + " " + "->" + " " + conc);
   }
- }
-} // fin class Graph
-var test = new Graph(7);
-var tab = ['A','B','C','D','E','F','G'];
+ } // fin printgraph
+} // fin class graph
+var tab = ['A','B','C','D','E','F','G','H','I','J','K','L','M'];
+var test = new Graph(13);
 for(var i = 0; i < tab.length; i++)
     test.addsommet(tab[i]);
     test.addchemin('A','B');
     test.addchemin('A','C');
     test.addchemin('A','D');
     test.addchemin('A','E');
-    test.addchemin('B','F');
-    test.addchemin('B','C');
-    test.addchemin('C','F');
-    test.addchemin('C','E');
-    test.addchemin('D','E');
-    test.addchemin('E','G');
-    test.addchemin('F','G');
+    test.addchemin('B','E');
+    test.addchemin('B','G');
+    test.addchemin('B','H');
+    test.addchemin('C','D');
+    test.addchemin('C','G');
+    test.addchemin('D','F');
+    test.addchemin('D','L');
+    test.addchemin('D','I');
+    test.addchemin('D','K');
+    test.addchemin('D','M');
+    test.addchemin('E','H');
+    test.addchemin('F','K');
+    test.addchemin('F','L');
+    test.addchemin('G','H');
+    test.addchemin('G','I');
+    test.addchemin('H','J');
+    test.addchemin('I','J');
+    test.addchemin('J','M');
+    test.addchemin('K','L');
+    test.addchemin('K','M');
+    console.log("This is your graph each sommet with it adjancy list :");
     test.printgraph();
